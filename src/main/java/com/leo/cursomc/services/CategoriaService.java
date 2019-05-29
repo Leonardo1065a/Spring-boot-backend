54efde0d395865a67b1fb9bc0107b4ce92944336
Leonardo Oliveira <leonardoplxy@gmail.com>
2019-05-29 12:21:15 -0300

    Deletando uma Categoria

--- src/main/java/com/leo/cursomc/services/CategoriaService.java
+++ src/main/java/com/leo/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.leo.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.leo.cursomc.domain.Categoria;
 import com.leo.cursomc.repositories.CategoriaRepository;
+import com.leo.cursomc.services.exception.DataIntegrityException;
 import com.leo.cursomc.services.exception.ObjNotFoundException;
 
 @Service
@@ -30,6 +32,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível, excluir uma categoria que possui produtos!");
+		}
 	}
 	
 }
--- src/main/java/com/leo/cursomc/services/exception/ObjNotFoundException.java
+++ src/main/java/com/leo/cursomc/services/exception/ObjNotFoundException.java
@@ -6,6 +6,7 @@ public class ObjNotFoundException extends RuntimeException {
 	public ObjNotFoundException(String msg) {
 		super(msg);
 	}
+	
 	public ObjNotFoundException(String msg, Throwable cause) {
 		super(msg, cause);
 	}
