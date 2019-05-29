04395092507361139d9aa7d0e5535d12d397ec27
Leonardo Oliveira <leonardoplxy@gmail.com>
2019-05-29 13:22:46 -0300

    Listando todas Categorias com DTO

--- src/main/java/com/leo/cursomc/services/CategoriaService.java
+++ src/main/java/com/leo/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.leo.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,10 +38,14 @@ public class CategoriaService {
 	public void delete(Integer id) {
 		find(id);
 		try {
-		repo.deleteById(id);
+			repo.deleteById(id);
 		}catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível, excluir uma categoria que possui produtos!");
 		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 	
 }
